03d11a4805162b3f245dc4116957a98f5084d615
sitaomin1994 <sitaomin@usc.edu>
2024-07-02 18:34:08 -0400

    parallel simple workflow

--- fedimpute/execution_environment/workflows/workflow.py
+++ fedimpute/execution_environment/workflows/workflow.py
@@ -153,3 +153,78 @@ class BaseWorkflow(ABC):
             )
 
             return evaluator.get_imp_quality(evaluation_results)
+
+    @staticmethod
+    def eval_and_track_parallel(
+        evaluator, tracker, clients_data, phase='round', epoch=0, log_eval=True, central_client=True
+    ):
+
+        ############################################################################################################
+        X_train_imps, X_train_origins, X_train_masks = [], [], []
+        for X_train_imp, X_train_origin, X_train_mask in clients_data:
+            X_train_imps.append(X_train_imp)
+            X_train_origins.append(X_train_origin)
+            X_train_masks.append(X_train_mask)
+
+        ############################################################################################################
+        # Initial evaluation and tracking
+        if phase == 'initial':
+
+            evaluation_results = evaluator.evaluate_imputation(
+                X_train_imps=X_train_imps, X_train_origins=X_train_origins, X_train_masks=X_train_masks,
+                central_client=central_client
+            )
+
+            tracker.record_initial(
+                data=X_train_origins, mask=X_train_masks, imp_quality=evaluation_results,
+            )
+
+            loguru.logger.info(
+                f"\nInitial: rmse - {evaluation_results['imp_rmse_avg']} ws - {evaluation_results['imp_ws_avg']}"
+            )
+
+            return None
+
+        ############################################################################################################
+        # Evaluation and tracking for each round
+        elif phase == 'round':
+
+            evaluation_results = evaluator.evaluate_imputation(
+                X_train_imps=X_train_imps, X_train_origins=X_train_origins, X_train_masks=X_train_masks,
+                central_client=central_client
+            )
+
+            tracker.record_round(
+                round_num=epoch + 1,
+                imp_quality=evaluation_results,
+                data=X_train_imps,
+                model_params=[],  # todo make it
+                other_info=[{} for _ in range(len(X_train_imps))]
+            )
+
+            if log_eval:
+                loguru.logger.info(
+                    f"Epoch {epoch}: rmse - {evaluation_results['imp_rmse_avg']} ws - {evaluation_results['imp_ws_avg']}"
+                )
+
+            return evaluator.get_imp_quality(evaluation_results)
+
+        ############################################################################################################
+        # Final evaluation and tracking
+        elif phase == 'final':
+
+            evaluation_results = evaluator.evaluate_imputation(
+                X_train_imps=X_train_imps, X_train_origins=X_train_origins,
+                X_train_masks=X_train_masks, central_client=central_client
+            )
+
+            tracker.record_final(
+                imp_quality=evaluation_results, data=X_train_imps, model_params=[],
+                other_info=[{} for _ in range(len(X_train_imps))]
+            )
+
+            loguru.logger.info(
+                f"Final: rmse - {evaluation_results['imp_rmse_avg']} ws - {evaluation_results['imp_ws_avg']}"
+            )
+
+            return evaluator.get_imp_quality(evaluation_results)
--- fedimpute/execution_environment/workflows/workflow_simple.py
+++ fedimpute/execution_environment/workflows/workflow_simple.py
@@ -7,6 +7,7 @@ from fedimpute.execution_environment.imputation.initial_imputation.initial_imput
 from fedimpute.execution_environment.utils.evaluator import Evaluator
 from fedimpute.execution_environment.utils.tracker import Tracker
 from .utils import formulate_centralized_client, update_clip_threshold
+from .parallel import client_process_func, server_process_func
 
 
 class WorkflowSimple(BaseWorkflow):
@@ -70,143 +71,78 @@ class WorkflowSimple(BaseWorkflow):
             self, clients: List[Client], server: Server, evaluator: Evaluator, tracker: Tracker
     ) -> Tracker:
 
-        """
-        Imputation workflow for MICE Multiprocessing Version
-        """
-
-        # Setup multiprocessing
-        mp.set_start_method('spawn')
-        client_pipes = [mp.Pipe() for _ in clients]
-        server_pipe, main_pipe = mp.Pipe()
-        evaluator_pipe, eval_main_pipe = mp.Pipe()
-        tracker_pipe, track_main_pipe = mp.Pipe()
-
-        # Start processes
-        client_processes = [mp.Process(target=client_process, args=(client, pipe[1])) for client, pipe in
-                            zip(clients, client_pipes)]
-        server_process = mp.Process(target=server_process,
-                                    args=(server, [pipe[0] for pipe in client_pipes], server_pipe))
-
-        evaluator_process = mp.Process(target=evaluator_process, args=(evaluator, evaluator_pipe, tracker_pipe))
-        tracker_process = mp.Process(target=tracker_process, args=(tracker, tracker_pipe))
-
-        for p in client_processes + [server_process, evaluator_process, tracker_process]:
-            p.start()
-
-        # Workflow Parameters
+        ############################################################################################################
+        # Initial Imputation and setup
         if server.fed_strategy.name == 'central':
             clients.append(formulate_centralized_client(clients))
-            # Add an additional pipe for the central client
-            central_pipe, central_client_pipe = mp.Pipe()
-            client_pipes.append((central_pipe, central_client_pipe))
-            central_process = mp.Process(target=client_process, args=(clients[-1], central_client_pipe))
-            central_process.start()
 
-        # Initial Imputation
-        for pipe, client in zip(client_pipes, clients):
-            pipe[0].send(("initial_impute", server.fed_strategy.initial_impute))
-
-        # Initial evaluation and tracking
-        self.eval_and_track(
-            evaluator, tracker, clients, phase='initial', central_client=server.fed_strategy.name == 'central'
+        clients = initial_imputation(server.fed_strategy.initial_impute, clients)
+        clients_data = [(client.X_train_imp, client.X_train, client.X_train_mask) for client in clients]
+        self.eval_and_track_parallel(
+            evaluator, tracker, clients_data, phase='initial', central_client=server.fed_strategy.name == 'central'
         )
 
+        ############################################################################################################
+        # Server and Client setup
+
+        client_pipes = [mp.Pipe() for _ in clients]
+        server_pipe, main_pipe = mp.Pipe()
+        client_processes = [mp.Process(
+            target=client_process_func, args=(client, pipe[1])) for client, pipe in zip(clients, client_pipes)
+        ]
+
+        server_process = mp.Process(
+            target=server_process_func, args=(server, [pipe[0] for pipe in client_pipes], server_pipe)
+        )
+
+        for p in client_processes + [server_process]:
+            p.start()
+
+        ############################################################################################################
         # Federated imputation
+        # Client fit local model
         fit_instruction = server.fed_strategy.fit_instruction([{} for _ in range(len(clients))])
         for pipe, client in zip(client_pipes, clients):
             train_params = {}
             train_params.update(fit_instruction[client.client_id])
             pipe[0].send(("fit_local", train_params))
 
+        # Server aggregation
         main_pipe.send("aggregate")
-        global_models = main_pipe.recv()
+        global_models, agg_res = main_pipe.recv()
 
+        # Client updata and local imputation
         for pipe, global_model in zip(client_pipes, global_models):
-            pipe[0].send(("update_and_impute", {'global_model': global_model, 'params': {}}))
+            pipe[0].send(("update_and_impute", {'global_model_params': global_model, 'params': {}}))
+
+        # Receive client imputation results
+        clients_data = [pipe[0].recv() for pipe in client_pipes]
 
         # Final Evaluation and Tracking and saving imputation model
-        self.eval_and_track(
-            evaluator, tracker, clients, phase='final', central_client=server.fed_strategy.name == 'central'
+        self.eval_and_track_parallel(
+            evaluator, tracker, clients_data, phase='final', central_client=server.fed_strategy.name == 'central'
         )
 
+        # Save Clients Model
         for pipe in client_pipes:
             pipe[0].send(("save_model", None))
 
-        # Terminate processes
-        for pipe in client_pipes:
+        # Terminate processes and update clients and server
+        for pipe, client in zip(client_pipes, clients):
             pipe[0].send(("terminate", None))
+            new_client = pipe[0].recv()
+            client.X_train_imp = new_client.X_train_imp
+            client.X_train = new_client.X_train
+            client.X_train_mask = new_client.X_train_mask
+            client.imputer = new_client.imputer
+            client.fed_strategy = new_client.fed_strategy
         main_pipe.send("terminate")
-
-        # Get final results from tracker
-        track_main_pipe.send(("get_results", None))
-        final_results = track_main_pipe.recv()
-        track_main_pipe.send(("terminate", None))
+        new_server = main_pipe.recv()
+        server.fed_strategy = new_server.fed_strategy
 
         # Join processes
-        for p in client_processes + [server_process, evaluator_process, tracker_process]:
+        for p in client_processes + [server_process]:
             p.join()
+            p.close()
 
-        if server.fed_strategy.name == 'central':
-            central_process.join()
-
-        return final_results
-
-
-def client_process(client, client_pipe):
-    while True:
-        command, data = client_pipe.recv()
-        if command == "initial_impute":
-            client.initial_impute(data)
-        elif command == "fit_local":
-            params, fit_res = client.fit_local_imp_model(data)
-            client_pipe.send((params, fit_res))
-        elif command == "update_and_impute":
-            client.update_local_imp_model(data['global_model'], params=data['params'])
-            client.local_imputation(params=data['params'])
-        elif command == "save_model":
-            client.save_imp_model(version='final')
-        elif command == "terminate":
-            break
-
-def server_process(server, client_pipes, server_pipe):
-    while True:
-        command = server_pipe.recv()
-        if command == "aggregate":
-            params_list, fit_rest_list = [], []
-            for pipe in client_pipes:
-                params, fit_res = pipe.recv()
-                params_list.append(params)
-                fit_rest_list.append(fit_res)
-            global_models, agg_res = server.fed_strategy.aggregate_parameters(params_list, fit_rest_list, {})
-            server_pipe.send(global_models)
-        elif command == "terminate":
-            break
-
-
-def evaluator_process(evaluator, evaluator_pipe, tracker_pipe):
-    while True:
-        command, data = evaluator_pipe.recv()
-        if command == "evaluate":
-            eval_results = evaluator.evaluate(
-                data["client_id"],
-                data["imputed_data"],
-                data["original_data"],
-                data["mask"]
-            )
-            tracker_pipe.send(("update", {
-                "client_id": data["client_id"],
-                "phase": data["phase"],
-                "results": eval_results
-            }))
-        elif command == "terminate":
-            break
-
-def tracker_process(tracker, tracker_pipe):
-    while True:
-        command, data = tracker_pipe.recv()
-        if command == "update":
-            tracker.update(data["client_id"], data["phase"], data["results"])
-        elif command == "get_results":
-            tracker_pipe.send(tracker.get_results())
-        elif command == "terminate":
-            break
+        return tracker
